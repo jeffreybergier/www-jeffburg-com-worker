baf58b62f8bf871a79597b29f0b804c6fa1f5e5d
jeffreybergier <jeffburg@jeffburg.com>
2023-10-14 18:25:38 +0900

    improved logging

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -64,9 +64,46 @@ function new404Response() {
     return new Response("404: URLの記載ミス", options);
 }
 
-function logObjectIfTrue(object, flag) {
-    if (flag) {
-        console.log(JSON.stringify(object, null, 4));
+/// Global Variable to Enable Extra Logging.
+/// This is configured by environment variable in `onRequest()`.
+var あぶないISDEBUG = true;
+
+function LOG(object) {
+    if (あぶないISDEBUG === false) { return; }
+    
+    const STACK_LINE_REGEX = /(\d+):(\d+)\)?$/;
+    var error;
+    var line = -2;
+    
+    /* // Failed attempt to find the line number by creating an exception
+       // https://kaihao.dev/posts/console-log-with-line-numbers
+    try {
+        throw new Error();
+    } catch (_error) {
+        error = _error;
+    }
+    try {
+        const stacks = err.stack.split('\\n');
+        const [, _line] = STACK_LINE_REGEX.exec(stacks[2]);
+        line = _line;
+    } catch (error) {
+        console.log(error);
+        line = -1;
+    }
+    */
+    
+    const kind = typeof object;
+    if (kind === "string" 
+     || kind === "number" 
+     || kind === "bigint"
+     || kind === "boolean"
+     || kind === "undefined"
+     || kind === "symbol"
+     || kind === null) 
+    {
+        console.log(`[${line}] ${object}`);
+    } else {
+        console.log(`[]${line}] ` + JSON.stringify(object, null, 4));
     }
 }
 
@@ -74,24 +111,24 @@ function logObjectIfTrue(object, flag) {
 export async function onRequest(context) {
     
     // Environment Variables
+    あぶないISDEBUG         = context.env.DEBUG === "true";
     const SPECIAL_REDIRECT = JSON.parse(context.env.SPECIAL_REDIRECT);
     const ALWAYS_REDIRECT  = context.env.ALWAYS_REDIRECT === "true";
-    const ISDEBUG          = context.env.DEBUG === "true";
     const DESTINATION      = context.env.DEST_BASE_URL;
     const INDEX            = context.env.INDEX_FILE_NAME;
     
-    logObjectIfTrue("Start: " + context.functionPath, ISDEBUG);
+    LOG("[Begin] " + context.functionPath);
     
     // Always safe fallback; Redirect
     if (ALWAYS_REDIRECT === true) {
-        logObjectIfTrue("Force Redirect: " + DESTINATION + context.functionPath, ISDEBUG);
+        LOG("[End] ALWAYS_REDIRECT: " + DESTINATION + context.functionPath);
         return Response.redirect(DESTINATION + context.functionPath, 301);
     }
     
     // Check for special redirect
     const redirect = SPECIAL_REDIRECT[context.functionPath];
     if (redirect != null) {
-        logObjectIfTrue("Special Redirect: " + redirect);
+        LOG("[End] SPECIAL_REDIRECT: " + redirect);
         return Response.redirect(redirect, 301);
     }
     
@@ -102,12 +139,13 @@ export async function onRequest(context) {
     );
     
     // Perform the request to the destination
-    logObjectIfTrue("Request: " + requestURLString, ISDEBUG);
+    LOG("[Internal] Request: " + requestURLString);
     const response = await fetch(requestURLString, newRequestOptionsFromRequest(context.request));
-    logObjectIfTrue(response.status + ": " + response.statusText, ISDEBUG);
+    LOG("[Internal] Response: " + response.status + ": " + response.headers.get("content-type"));
 
     // Guard statement to bail if the response code is not success
     if (isSuccessStatusCodeNumber(response.status) === false) {
+        LOG("[End] Return Internal Response: Bad Status Code");
         return response; 
     }
     
@@ -122,7 +160,7 @@ export async function onRequest(context) {
         // Github sets content security policy, so it needs to be deleted or changed
         headers.delete("content-security-policy");
         const options = newResponseOptionsFromResponse(response, headers);
-        logObjectIfTrue(headers.get("content-type") + ": Modified Content Type", ISDEBUG);
+        LOG("[End] Return Modified MIME: " + headers.get("content-type"));
         return new Response(response.body, options);
     } else if (responseFileExtension === "css") {
         const headers = newHeadersByModifyingMIMEType(
@@ -130,7 +168,7 @@ export async function onRequest(context) {
             "text/css; charset=UTF-8"
         );
         const options = newResponseOptionsFromResponse(response, headers);
-        logObjectIfTrue(headers.get("content-type") + ": Modified Content Type", ISDEBUG);
+        LOG("[End] Return Modified MIME: " + headers.get("content-type"));
         return new Response(response.body, options);
     } else if (responseFileExtension === "js") {
         const headers = newHeadersByModifyingMIMEType(
@@ -138,14 +176,14 @@ export async function onRequest(context) {
             "text/javascript; charset=UTF-8"
         );
         const options = newResponseOptionsFromResponse(response, headers);
-        logObjectIfTrue(headers.get("content-type") + ": Modified Content Type", ISDEBUG);
+        LOG("[End] Return Modified MIME: " + headers.get("content-type"));
         return new Response(response.body, options);
     } else if (responseFileExtension === "php") {
-        logObjectIfTrue("Force 404: " + context.functionPath, ISDEBUG);
+        LOG("[End] Force 404: Requested PHP" + headers.get("content-type"));
         return new404Response();
     } else {
         // If not HTML/CSS/JS then the MIME type set by Github should be correct
-        logObjectIfTrue(response.headers.get("content-type") + ": Original Content Type", ISDEBUG);
+        LOG("[End] Return Original MIME: " + response.headers.get("content-type"));
         return response
     }
 }
